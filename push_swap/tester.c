8530a9368dfd694ea59f4fbe76aa29bb1062f713
Alexandre Teixeira <alexandremagteixeira@gmail.com>
2025-02-26 23:21:14 +0000

    adding cleanup function to tester in push_swap

--- push_swap/tester.c
+++ push_swap/tester.c
@@ -6,7 +6,7 @@
 /*   By: paalexan <paalexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 22:00:00 by paalexan          #+#    #+#             */
-/*   Updated: 2025/02/21 17:12:24 by paalexan         ###   ########.fr       */
+/*   Updated: 2025/02/26 23:20:47 by paalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ static void	print_stack(t_stack *stack)
 		stack = stack->next;
 	}
 	ft_printf("\n");
+}
+
+static void	free_lst(t_stack **a, t_stack **b)
+{
+	ft_lst_clear_ps(a);
+	ft_lst_clear_ps(b);
 }
 
 static int	apply_move(char *move, t_stack **a, t_stack **b)
@@ -54,21 +60,26 @@ static int	apply_move(char *move, t_stack **a, t_stack **b)
 static int	read_and_apply_moves(t_stack **a, t_stack **b)
 {
 	char	*move;
+	bool	flag;
 
+	flag = true;
 	move = get_next_line(0);
-	while (move)
+	while (move || flag)
 	{
+		flag = false;
+		if (!move)
+			break;
 		if (!apply_move(move, a, b))
 		{
 			free(move);
-			ft_lst_clear_ps(a);
-			ft_lst_clear_ps(b);
+			get_next_line(-1);
 			ft_putstr_fd("Error\n", 2);
 			return (0);
 		}
 		free(move);
 		move = get_next_line(0);
 	}
+	free(move);
 	return (1);
 }
 
@@ -81,19 +92,20 @@ int	main(int argc, char **argv)
 	b = NULL;
 	if (argc < 2)
 		return (0);
-	if (!parse_args(&a, argc, argv))
+	if (parse_args(&a, argc, argv) != 1)
+	{
+		free_lst(&a, &b);
 		return (1);
+	}
 	if (!read_and_apply_moves(&a, &b))
 	{
-		ft_lst_clear_ps(&a);
-		ft_lst_clear_ps(&b);
+		free_lst(&a, &b);
 		return (1);
 	}
 	if (is_sorted(a) && !b)
 		ft_putstr_fd("OK\n", 1);
 	else
 		print_stack(a);
-	ft_lst_clear_ps(&a);
-	ft_lst_clear_ps(&b);
+	free_lst(&a, &b);
 	return (0);
 }
