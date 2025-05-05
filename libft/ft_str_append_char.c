d964d531c944063494332752b79c9dc60dcee650
Alexandre Teixeira <alexandremagteixeira@gmail.com>
2025-05-05 18:16:11 +0100

    add safety guards on ft_str_append_char

--- libft/ft_str_append_char.c
+++ libft/ft_str_append_char.c
@@ -6,7 +6,7 @@
 /*   By: paalexan <paalexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 00:44:34 by paalexan          #+#    #+#             */
-/*   Updated: 2025/04/11 15:03:19 by paalexan         ###   ########.fr       */
+/*   Updated: 2025/05/05 18:15:10 by paalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	ft_str_append_char(char **res, const char c)
 	tmp[0] = c;
 	tmp[1] = '\0';
 	joined = ft_strjoin(*res, tmp);
+	if (!joined)
+		return ;
 	free(*res);
 	*res = joined;
 }
